e44b41f6dd11c06111fc568e48535940db7da652
Abhishek Vijayakumar <abhishev@andrew.cmu.edu>
2020-01-05 20:10:30 -0600

    fixed stderr settings

--- lib.py
+++ lib.py
@@ -1,4 +1,4 @@
-def format_output(content: str, stdout: str = "", stderr: str = "") -> str:
+def format_output(content: str, stdout: str = None, stderr: str = None) -> str:
     """
     Formats the input and response from a command for display in Discord.
     :param content: The input message from the user.
@@ -10,7 +10,7 @@ def format_output(content: str, stdout: str = "", stderr: str = "") -> str:
     output += f"$ {content}\n"
     output += f"{stdout}\n"
     # TODO: fix this formatting
-    if stderr is not "":
+    if stderr is not None:
         output += f"stderr: {stderr}\n"
     output += "```"
     return output
